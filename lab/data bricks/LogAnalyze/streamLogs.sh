064cbd86fd109ddfa59b0ee22fe1c4a5fe71f923
yclee0418 <whitelee@gmail.com>
2018-03-26 22:20:24 +0530

    modify nectar command

--- lab/data bricks/LogAnalyze/streamLogs.sh
+++ lab/data bricks/LogAnalyze/streamLogs.sh
@@ -5,11 +5,11 @@ if [ -z "$1" ]; then
         NCSRV="localhost"
         PORT="9999"
 fi
-#send content to nc localhost 9999, line by line
-#received by nc -lk 9999
+#send content to nc -lk 9999, line by line
+#received by nc localhost 9999
 cat data/apache.access.log | while read line; do
         echo "$line"
-        sleep 0.1
-done | nc $NCSRV $PORT
+        sleep 0.5
+done | nc -l $PORT
 
 
