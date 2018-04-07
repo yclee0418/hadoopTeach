a99d722dd50f7434d4c0df7999178bb839fa4ed3
yclee0418 <whitelee@gmail.com>
2018-04-07 22:06:00 +0530

    start ipython notebook with spark sql

---
+++ lab/data bricks/twitter_classifier/startNotebook3.sh
@@ -0,0 +1,6 @@
+export PATH=$PATH:/Users/yungchuanlee/spark-2.2.1/bin
+export PATH=$PATH:/Users/yungchuanlee/anaconda3/bin
+export ANACONDA_PATH=/Users/yungchuanlee/anaconda3
+export PYSPARK_DRIVER_PYTHON=$ANACONDA_PATH/bin/ipython
+export PYSPARK_PYTHON=$ANACONDA_PATH/bin/python
+PYSPARK_DRIVER_PYTHON=ipython PYSPARK_DRIVER_PYTHON_OPTS="notebook" pyspark
